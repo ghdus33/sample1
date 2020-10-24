4d0fd6ab4c6b947922725081219fb090eb4275b2
DESKTOP-FKF0B4K\ghdus <ghdus33@gmail.com>
2020-10-24 16:47:35 +0900

    테스트용 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/**
+ * 
+ */
+router.get('/test', (req, res,next) => {
+
+  console.log("router test called..") // 콘솔에 호출
+  res.json("test")  // 요청자에게 응답..
+})
 
 module.exports = router;
